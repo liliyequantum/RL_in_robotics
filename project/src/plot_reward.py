743879b100191abc7f8494ccb359862bfdb7cd39
liliyequantum <liliye@asu.edu>
2024-11-14 08:42:18 -0700

    update results

--- project/src/plot_reward.py
+++ project/src/plot_reward.py
@@ -3,17 +3,21 @@ import matplotlib.pyplot as plt
 
 # Load the data from ppo.txt
 data = np.loadtxt('./record/complex_env_basic_reward_ppo/ppo.txt')
+len_data = len(data)
 #threshold = 20
 #filtered_data = data[(data > -threshold) & (data < threshold)]
+#n = 10
+#averaged_data = np.mean(data[:len_data // n * n].reshape(-1, n), axis=1)
 
 # Plot the data
 plt.figure(figsize=(8,6))
 plt.plot(data)
+#plt.plot(np.arange(0,len_data,n), data)
 plt.xlabel('Episodes',fontsize = 18)
 plt.ylabel('Ave Rewards',fontsize=18)
 #plt.title('Data from ppo.txt')
 plt.tick_params(labelsize=18)  # Set tick label size for both axes
-plt.ylim([-5,10])
+plt.ylim([-10,30])
 #plt.yticks([-5,0,5,10,15])
 # Save the figure
 plt.tight_layout()
